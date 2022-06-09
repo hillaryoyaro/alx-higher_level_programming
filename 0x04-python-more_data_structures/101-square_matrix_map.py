402f46ae9d7a13fcead50cc91a06af798fc1b2f5
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-06-09 15:31:00 -0700

    square_matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
-def multiple_returns(sentence):
-    if not sentence:
-        sentence = None
-    if sentence:
-        sen_len = len(sentence)
-    else:
-        sen_len = 0
-    return (sen_len, sentence if not sentence else sentence[:1])
+def square_matrix_map(matrix=[]):
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
