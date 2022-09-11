6205a8f757d5d151b07c1cb459c4eb9b4a2e3bfe
Hillary Oyaro <hillaryoyaroh@gmail.com>
2022-09-11 15:06:37 -0700

    Filter the state start with N in upper

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N"""
-import MySQLdb
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+# Usage: ./1-filter_states.py 
+<mysql username> 
+mysql password
+database name
+"""
 import sys
-
-
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
