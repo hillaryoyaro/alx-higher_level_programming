716507196022c740ae419be2c59c9dd2e4b6eb75
hillaryoyaro <hillaryoyaroh@gmail.com>
2022-09-17 10:32:48 +0300

    Model Cities

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Definition of class City """
+""" Definition of class City. """
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
